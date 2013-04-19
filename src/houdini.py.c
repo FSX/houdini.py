e73be5b45f5c3cf9f0f9eea0fb490f4e72e0fdec
FSX <frank@61924.nl>
2013-04-19 16:41:01 +0200

    Update to latest Houdini C code and rework Python module.

--- src/houdini.py.c
+++ src/houdini.py.c
@@ -4,122 +4,56 @@
 #include "houdini/houdini.h"
 
 
-#define PRE_ESCAPE \
-    PyObject *py_result;\
-    unsigned char *text;\
-    size_t text_size;\
-    if (!PyArg_ParseTuple(args, "s#", &text, &text_size))\
+#define HTML_ESCAPE(buf, str, len) houdini_escape_html0(buf, str, len, 0)
+#define HTML_ESCAPE_SECURE(buf, str, len) houdini_escape_html0(buf, str, len, 1)
+
+#define HOUDINI_FUNC(name, body) static PyObject * name (PyObject *self, PyObject *args) { body; }
+#define HOUDINI_WRAPPER(escape_function) {\
+    PyObject *py_str;\
+    if (!PyArg_ParseTuple(args, "O", &py_str)) {\
         return NULL;\
-    struct buf *ob = bufnew(128);
-
-
-#define POST_ESCAPE \
-    py_result = Py_BuildValue("s#", ob->data, (int)ob->size);\
-    bufrelease(ob);\
-    return py_result;
-
-
-static PyObject *
-py_escape_html(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_escape_html0(ob, text, text_size, 0);
-    POST_ESCAPE;
+    }\
+    if (!PyUnicode_Check(py_str)) {\
+        PyErr_SetString(PyExc_TypeError, "Argument must be a string.");\
+        return NULL;\
+    }\
+    gh_buf buf = GH_BUF_INIT;\
+    char *str = PyUnicode_AsUTF8(py_str);\
+    if ( escape_function (&buf, (const uint8_t *) str, strlen(str)))\
+    {\
+        Py_DECREF(py_str);\
+        py_str = Py_BuildValue("s#", buf.ptr, (int) buf.size);\
+        gh_buf_free(&buf);\
+    }\
+    return py_str;\
 }
 
 
-static PyObject *
-py_unescape_html(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_unescape_html(ob, text, text_size);
-    POST_ESCAPE;
-}
-
-
-static PyObject *
-py_escape_href(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_escape_href(ob, text, text_size);
-    POST_ESCAPE;
-}
-
-
-static PyObject *
-py_escape_xml(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_escape_xml(ob, text, text_size);
-    POST_ESCAPE;
-}
-
-
-static PyObject *
-py_escape_uri(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_escape_uri(ob, text, text_size);
-    POST_ESCAPE;
-}
-
-
-static PyObject *
-py_escape_url(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_escape_url(ob, text, text_size);
-    POST_ESCAPE;
-}
-
-
-static PyObject *
-py_unescape_uri(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_unescape_uri(ob, text, text_size);
-    POST_ESCAPE;
-}
-
-
-static PyObject *
-py_unescape_url(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_unescape_url(ob, text, text_size);
-    POST_ESCAPE;
-}
-
-
-static PyObject *
-py_escape_js(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_escape_js(ob, text, text_size);
-    POST_ESCAPE;
-}
-
-
-static PyObject *
-py_unescape_js(PyObject *self, PyObject *args)
-{
-    PRE_ESCAPE;
-    houdini_unescape_js(ob, text, text_size);
-    POST_ESCAPE;
-}
+HOUDINI_FUNC(py_escape_html, HOUDINI_WRAPPER(HTML_ESCAPE));
+HOUDINI_FUNC(py_escape_html_secure, HOUDINI_WRAPPER(HTML_ESCAPE_SECURE));
+HOUDINI_FUNC(py_unescape_html, HOUDINI_WRAPPER(houdini_unescape_html));
+HOUDINI_FUNC(py_escape_xml, HOUDINI_WRAPPER(houdini_escape_xml));
+HOUDINI_FUNC(py_escape_uri, HOUDINI_WRAPPER(houdini_escape_uri));
+HOUDINI_FUNC(py_escape_url, HOUDINI_WRAPPER(houdini_escape_url));
+HOUDINI_FUNC(py_escape_href, HOUDINI_WRAPPER(houdini_escape_href));
+HOUDINI_FUNC(py_unescape_uri, HOUDINI_WRAPPER(houdini_unescape_uri));
+HOUDINI_FUNC(py_unescape_url, HOUDINI_WRAPPER(houdini_unescape_url));
+HOUDINI_FUNC(py_escape_js, HOUDINI_WRAPPER(houdini_escape_js));
+HOUDINI_FUNC(py_unescape_js, HOUDINI_WRAPPER(houdini_unescape_js));
 
 
 static PyMethodDef houdini_methods[] = {
-    {"escape_html", py_escape_html, METH_VARARGS, "Escape HTML"},
-    {"unescape_html", py_unescape_html, METH_VARARGS, "Unescape HTML"},
-    {"escape_href", py_escape_href, METH_VARARGS, "Escape HREF"},
-    {"escape_xml", py_escape_xml, METH_VARARGS, "Escape XML"},
-    {"escape_uri", py_escape_uri, METH_VARARGS, "Escape URI"},
-    {"escape_url", py_escape_url, METH_VARARGS, "Escape URL"},
-    {"unescape_uri", py_unescape_uri, METH_VARARGS, "Unescape URI"},
-    {"unescape_url", py_unescape_url, METH_VARARGS, "Unescape URL"},
-    {"escape_js", py_escape_js, METH_VARARGS, "Escape JavaScript"},
-    {"unescape_js", py_unescape_js, METH_VARARGS, "Unescape JavaScript"},
+    {"escape_html", py_escape_html, METH_VARARGS, NULL},
+    {"escape_html_secure", py_escape_html_secure, METH_VARARGS, NULL},
+    {"unescape_html", py_unescape_html, METH_VARARGS, NULL},
+    {"escape_xml", py_escape_xml, METH_VARARGS, NULL},
+    {"escape_uri", py_escape_uri, METH_VARARGS, NULL},
+    {"escape_url", py_escape_url, METH_VARARGS, NULL},
+    {"escape_href", py_escape_href, METH_VARARGS, NULL},
+    {"unescape_uri", py_unescape_uri, METH_VARARGS, NULL},
+    {"unescape_url", py_unescape_url, METH_VARARGS, NULL},
+    {"escape_js", py_escape_js, METH_VARARGS, NULL},
+    {"unescape_js", py_unescape_js, METH_VARARGS, NULL},
     {NULL, NULL, 0, NULL} /* Sentinel */
 };
 
